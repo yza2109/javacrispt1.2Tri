6113072445a1f6683569c7d9034867e728538f3c
yza2109 <yzadora.assuncao@escola.pr.gov.br>
2026-03-13 07:54:09 -0300

    create: //13032026

---
+++ 13032026.js
@@ -0,0 +1,11 @@
+let lucro = 30;
+let lucro2 = 10;
+
+function soma(a,b){
+    return   a + b
+}
+soma(lucro,lucro2)
+
+function resultadoDivisaoSocio(lucro,lucro2){
+    return soma(lucro,lucro2)/2
+}
