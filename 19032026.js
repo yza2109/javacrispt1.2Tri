cd12c3c9d41305278d75843f03bbd8cee8889945
yza2109 <yzadora.assuncao@escola.pr.gov.br>
2026-03-20 07:53:47 -0300

    uptd

--- 19032026.js
+++ 19032026.js
@@ -7,3 +7,24 @@ let numero = 30
 let decimal = 20.30
 // String
 let nome = "Não sei, você não sabe?"
+let nome1 = "NãO SEI, VOCÊ NãO SABE?"
+
+console.log(nome === nome1); //false
+
+
+const inputMinusculo = nome1.toLowerCase();
+
+console.log(nome === inputMinuslo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 carateres
+
+
+// Boleanos
+
+let nv = true;
+let nz = false;
+
+if(nv!==nz){
+    console.log('nv é diferente, pois ele é ${nv}')
+}
